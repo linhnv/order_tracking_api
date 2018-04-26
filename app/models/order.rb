f1e6913f0a40282357e1e2f88eb187c4e35c7906
Linh Nguyen <nguyenvanlinh.bk@gmail.com>
2018-04-27 00:40:54 +0700

    Added order_item model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :customer
+  has_many :order_items
 
   enum state: %i(requested processing delivering completed returned)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_26_172445) do
+ActiveRecord::Schema.define(version: 2018_04_26_173601) do
 
   create_table "customers", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,17 @@ ActiveRecord::Schema.define(version: 2018_04_26_172445) do
     t.string "external_urls"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "product_id"
+    t.decimal "price"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
